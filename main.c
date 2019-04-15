404407b0e084bd5fe9c9b311c308ce18a84e3f53
regan bridger <c3305643@uon.edu.au>
2019-04-15 07:54:49 +0000

    substitution cipher encryption given

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int scipherDto();   // Decryption of a message encrypted with a substitution cip
     
 int main() {
     
-    int x = 1, k = 5;
+    int x = 3, k = 5;
     
     // use the terminal to input a value for x and k or change the value in the code
     
@@ -28,8 +28,14 @@ int main() {
     printf("6. Decrypt a message encrypted with a substitution cipher given cipher text only\n");
     scanf("%d", &x);
     
-    printf("rotation amount for rotation cipher: \n");
-    scanf("%d", &k);
+    if (x == 2 || x == 1){
+        printf("rotation amount for rotation cipher: \n");
+        scanf("%d", &k);
+    }
+    else if (x == 4 || x == 3){
+        printf("Write subsitution key into subKey.txt file in the form ABCDEFGHILKLMNOPQRSTUVWXYZ\n");
+    }
+    
     
     switch(x){
         case 1:         // encryption of a message with a rotation cipher given message and rotation amount
@@ -41,7 +47,7 @@ int main() {
             
         case 2:         // Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount
             
-            rcipherD();
+            rcipherD(k);
             
             break;
             
@@ -138,8 +144,7 @@ int rcipherE(int k){
 	           
 	           str[x] = ((str[x] + k) % 26) + 52;
 	           
-	        }
-	        
+	        }	        
 	        
 	    }
 	    
@@ -153,16 +158,166 @@ int rcipherE(int k){
     
 }
 
-int rcipherD(){
+int rcipherD(int k){
     
-    printf("yess");
+    FILE *size;        // to initially find the size the array needs to be
+	                   // store decrypted message in "decryptedMessage.txt" for encrypting
+	size = fopen("encryptedMessage.txt", "r");
+
+	int x = 0;
+	char c;
+	
+	while (feof(size) == 0){    
+	    
+    	fscanf(size, "%c", &c);
+        
+	    x++;
+	}
+	
+	fclose(size);
+
+    FILE *input;        // write the file to an array
+    
+	input = fopen("encryptedMessage.txt", "r");
+	
+    char str[x];
+    int s = x-1;
+    x = 0;
+    
+    while (feof(input) == 0){
+	    
+	    
+    	fscanf(input, "%c", &c);
+    	
+    	str[x] = c;
+        str[s] = '\0';
+              
+    	
+        
+	    x++;
+	}
+	
+	fclose(input);
+	
+	x = 0;
+	
+	FILE *output;
+	output = fopen("decryptedMessage.txt", "w");   
+	
+	for (x = 0; x < s; x++){
+	    
+	    
+	    
+	    if (str[x] > 64 && str[x] < 91){
+	        
+	        if (str[x] - k > 64){
+                str[x] = str[x] - k;
+                
+            }
+            else if(x - k < 65){
+                str[x] = ((str[x] - k) % 26) + 78;               
+            } 
+            
+	    }
+	    
+	    fprintf(output, "%c", str[x]);	    
+	}
+	
+	fclose(output);
     
     return 0;
 }
 
 int scipherE(){
     
-    printf("yis");
+    FILE *key;
+    key = fopen("subKey.txt", "r");
+    
+    int x = 0;
+    char str[26];
+    char c;
+    
+    printf("the key is: ");
+    for (x = 0; x < 26; x++){
+        
+        fscanf(key, "%c", &c);
+        str[x] = c;
+        printf("%c", str[x]);
+        
+    }
+    
+    fclose(key);
+    
+    FILE *size;        // to initially find the size the array needs to be
+	                   // store decrypted message in "decryptedMessage.txt" for encrypting
+	size = fopen("decryptedMessage.txt", "r");
+	
+	x = 0;
+	
+	while (feof(size) == 0){    
+	    
+    	fscanf(size, "%c", &c);
+        
+	    x++;
+	}
+	
+	fclose(size);
+
+    FILE *input;        // write the file to an array
+    
+	input = fopen("decryptedMessage.txt", "r");
+	
+    char str2[x];
+    int s = x-1;
+    x = 0;
+    printf("\n");
+    printf("input: ");
+    while (feof(input) == 0){
+	    
+	    
+    	fscanf(input, "%c", &c);
+    	
+    	str2[x] = c;
+    	
+    	if(str2[x] > 96 && str2[x] < 123){
+            
+    	    str2[x] = c - 32;
+            str2[s] = '\0';             
+    	}
+    	
+        printf("%c", str2[x]);
+	    x++;
+	}
+	
+	fclose(input);
+	
+	int a, b;
+	
+	printf("\n");
+	
+	FILE *output;
+	output = fopen("encryptedMessage.txt", "w");
+	
+	printf("output: ");
+	
+	for (x = 0; x < s; x++){
+	    
+	    for(b = 0; b < 26; b++){
+	        
+	        a = b + 65;
+	        
+	        if (str2[x] == a){
+	            str2[x] = str[b];
+	            break;
+	        }	        
+	    }
+	    
+	    fprintf(output, "%c", str2[x]);
+	    printf("%c", str2[x]);
+	}
+	
+	fclose(output);
+    
     
     return 0;
 }
