289332851dbe641c375258257a650ca194214a56
regan bridger <c3305643@uon.edu.au>
2019-04-15 02:54:59 +0000

    Encrypts message using rotation cipher

--- main.c
+++ main.c
@@ -15,7 +15,9 @@ int scipherDto();   // Decryption of a message encrypted with a substitution cip
     
 int main() {
     
+    int x = 1;
     
+    // use the terminal to input a value for x or change the value in the code
     
     printf("CHOOSE DESIRED OPTION:\n");
     printf("1. Encrypt a message with a rotation cipher given the message and rotation amount\n");
@@ -24,14 +26,15 @@ int main() {
     printf("4. Decrypt a message encrypted with a substitution cipher given cipher text and substitutions\n");
     printf("5. Decrypt a message encrypted with a rotation cipher given cipher text only\n");
     printf("6. Decrypt a message encrypted with a substitution cipher given cipher text only\n");
+    scanf("%d", &x);
     
     
-    const int x = 1;
     
     switch(x){
         case 1:         // encryption of a message with a rotation cipher given message and rotation amount
-        
+            
             rcipherE();
+            
             
             break;
             
@@ -72,34 +75,101 @@ int main() {
 
 int rcipherE(){
     
-    int a = 0;
+    FILE *size;        // to initially find the size the array needs to be
+	                   // store decrypted message in "decryptedMessage.txt" for encrypting
+	size = fopen("decryptedMessage.txt", "r");
+
+	int x = 0;
+	char c;
+	
+	while (feof(size) == 0){    
+	    
+    	fscanf(size, "%c", &c);
+        
+	    x++;
+	}
+	
+	fclose(size);
+
+    FILE *input;        // write the file to an array
     
-    FILE *input;
-    input =  fopen("decryptedMessage.txt", "r");
+	input = fopen("decryptedMessage.txt", "r");
+	
+    char str[x];
+    int s = x-1;
+    x = 0;
     
     while (feof(input) == 0){
+	    
+	    
+    	fscanf(input, "%c", &c);
+    	
+    	str[x] = c;
+    	
+    	if(str[x] > 96 && str[x] < 123){
+            
+    	    str[x] = c - 32;
+            str[s] = '\0';
+              
+    	}
         
-    a++;     
+	    x++;
+	}
+	
+	fclose(input);
+	
+	x = 0;
+	
+	int k = 5;
+	printf("rotate by: \n");
+	scanf("%d\n", &k);
+	    
+	for (x = 0; x < s; x++){
+	    
+	    
+	    
+	    if (str[x] > 64 && str[x] < 91){
+	        
+	        if (str[x] + k < 91){
+	            
+	            str[x] = str[x] + k;
+	        }
+	        else if(str[x] + k > 90){
+	           
+	           str[x] = ((str[x] + k) % 26) + 52;
+	           
+	        }
+	        
+	        
+	    }
+	    
+	    printf("%c", str[x]);
+	    
+	}
+	
+	
     
-    }
-    printf("%d", a);
+    return 0; 
     
-    return 0;
 }
 
 int rcipherD(){
     
+    printf("yess");
     
     return 0;
 }
 
 int scipherE(){
     
+    printf("yis");
     
     return 0;
 }
 
 int scipherD(){
+    
+    printf("neg");
     
     
     return 0;
@@ -107,12 +177,15 @@ int scipherD(){
 
 int rcipherDto(){
     
+    printf("hanoi");
+    
     
     return 0;
 }
 
 int scipherDto(){
     
+    printf("no");
     
     return 0;
 }
